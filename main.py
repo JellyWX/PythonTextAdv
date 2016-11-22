f46ceaa999e0baf788aeea1cb6f9d9cdef58005d
PythonJellyfish <judewrs@gmail.com>
2016-11-22 22:12:48 +0000

    made it so you can pick up carriable objects from containers and store them in an inventory. added the commands 'scan', 'search' and 'collect' which do similar fucntions to the most part to find and take items.

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ class Room():
   def __init__(self, n):
     self.name = n
     self.exits = []
-    self.contents = ''
+    self.contents = []
     self.locked = False
     print('N:spawned in a room. name:' + self.name + ', exits:' + str(self.exits))
 
@@ -20,30 +20,49 @@ class Room():
   def addExit(self, e):
     self.exits.append(e)
 
+  def addContainer(self, c):
+    self.contents.append(c.name)
+
+  def search(self):
+    print('N:rooms contents are ' + str(self.contents))
+
 class Container(object):
   room = ''
   name = ''
-  subcontainers = []
+  contents = ''
 
   def __init__(self, r, n):
     self.room = r
     self.name = n
-    self.subcontainers = []
-    print('N:created container ' + self.name + 'with no contents at ' + self.room)
+    self.contents = []
+    print('N:created container ' + self.name + ' with no contents at ' + self.room.name)
+    self.room.addContainer(self)
+
+  def addContent(self, c):
+    self.contents.append(c.name)
+
+  def removeContent(self, c):
+    self.contents.remove(c.name)
+
+  def search(self):
+    print('N:containers contents are ' + str(self.contents))
 
 class Item(object):
   name = ''
   carriable = True
   container = ''
 
+
   def __init__(self, c, n):
     self.container = c
     self.name = n
     self.carriable = True
-    print('N:created a new ' + self.name + ' in ' + self.container)
+    print('N:created a new ' + self.name + ' in ' + self.container.name)
+    self.container.addContent(self)
 
-  def hide(self):
-    self.container = ''
+  def collect(self, collector):
+    self.container.removeContent(self)
+    self.container = collector
 
 class PlayerObj(object):
   health = 0
@@ -57,8 +76,8 @@ class PlayerObj(object):
     print('N:new player object spawned in at ' + self.room.name)
 
   def addItem(self, i):
-    self.inventory.append(i)
-    i.hide()
+    self.inventory.append(i.name)
+    i.collect(self)
 
   def doAction(self, action):
     action = action.strip(' ')
@@ -76,12 +95,37 @@ class PlayerObj(object):
 
         else:
           print('Room not available for travel. Use `> rooms` to find available rooms.')
+
       except:
         print('Room name entered not found. USAGE: `> room <name>` (Must be a connected room. Find connected rooms using `> rooms`)')
+
     elif action.split(' ')[0] == 'collect':
       try:
-        if eval(action.split(' ')[1]).carriable == True:
-          self.inventory.addItem(eval(action.split(' ')[1]))
+        if eval(action.split(' ')[1]).name in self.room.contents:
+          if eval(action.split(' ')[2]).name in eval(action.split(' ')[1]).contents:
+            if eval(action.split(' ')[2]).carriable == True:
+              self.addItem(eval(action.split(' ')[2]))
+            else:
+              print('Item cant be carried')
+          else:
+            print('Item couldnt be found')
+        else:
+          print('Couldnt find container')
+      except:
+        print('Failed to collect said item')
+
+    elif action.split(' ')[0] == 'scan':
+      self.room.search()
+
+    elif action.split(' ')[0] == 'search':
+      try:
+        eval(action.split(' ')[1]).search()
+      except:
+        print('Container not found')
+
+    elif action.split(' ')[0] == 'inventory':
+      print(self.inventory)
+
     elif action == 'exit':
       exit()
 
@@ -90,6 +134,9 @@ landing = Room('landing')
 stairs = Room('stairs')
 hall = Room('hall')
 kitchen = Room('kitchen')
+
+shelf = Container(hall, 'shelf')
+key = Item(shelf, 'key')
 
 bedroom.addExit('landing')
 
@@ -103,10 +150,9 @@ hall.addExit('kitchen')
 hall.addExit('stairs')
 
 kitchen.addExit('hall')
-
 kitchen.locked = True
 
-player = PlayerObj(bedroom, 100)
+player = PlayerObj(hall, 100)
 
 playing = True
 
